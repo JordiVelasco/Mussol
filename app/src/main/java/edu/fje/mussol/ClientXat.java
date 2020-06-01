37e09ab5a3b61d0e7af469cea02c59a5782bb5ec
Jordi Velasco <030177.clot@fje.edu>
2020-06-01 18:29:27 +0200

    bug (Servidor ple | FINAL) fixed v0.3

--- app/src/main/java/edu/fje/mussol/ClientXat.java
+++ app/src/main/java/edu/fje/mussol/ClientXat.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.dynamic.IFragmentWrapper;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -16,11 +20,12 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class ClientXat extends AppCompatActivity {
 
     private Button boto;
-    private TextView missatges, missatges2;
+    private TextView missatges;
     private EditText entrada;
     private PrintWriter sortida;
     private Socket socol = null;
@@ -34,7 +39,6 @@ public class ClientXat extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         boto = findViewById(R.id.boto);
         missatges = findViewById(R.id.missatges);
-        missatges2 = findViewById(R.id.missatges);
         entrada = findViewById(R.id.entrada);
         byte[] addr = new byte[4];
         addr[0] = (byte) 192;
@@ -60,7 +64,7 @@ public class ClientXat extends AppCompatActivity {
                     try {
                         OutputStream outStream = socol.getOutputStream();
                         PrintWriter sortida = new PrintWriter(outStream, true);
-                        sortida.println("User 1: " + entrada.getText().toString());
+                        sortida.println(entrada.getText().toString());
                     } catch (UnknownHostException e) {
                         System.out.println("host desconegut");
                         e.printStackTrace();
@@ -84,17 +88,27 @@ public class ClientXat extends AppCompatActivity {
         public void run() {
             try {
                 input = new BufferedReader(new InputStreamReader(socol.getInputStream()));
-                while (true) {
+                final boolean[] fet = {false};
+                while (!fet[0]) {
                     final String resposta = input.readLine();
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            missatges.append(resposta + "\n");
+                            if (resposta == null || resposta.equals("SERVIDOR PLE")){
+                                missatges.setText("Si es vol tornar a conectar, pot sortir i tornar a entrar");
+                                try { socol.close(); fet[0] = true;}
+                                catch (IOException e) { e.printStackTrace(); }
+                            } else {
+                                missatges.append(resposta + "\n");
+                            }
                         }
                     });
                 }
             }
-            catch (UnknownHostException e) { System.out.println("host desconegut"); e.printStackTrace(); }
+            catch (UnknownHostException e) {
+                System.out.println("host desconegut");
+                e.printStackTrace();
+            }
             catch (IOException e) { System.out.println("problemes E/S"); }
             catch (Exception e) {}
         }
